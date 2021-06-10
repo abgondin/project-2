ed1cfe8bfb9286c9d812536fd581ddd9893508b9
teals <tealiie@live.com>
2021-06-10 21:04:26 +1200

    populate data table

--- webpage/data/app.js
+++ webpage/data/app.js
@@ -1,1 +1,18 @@
-    
+
+// import the data
+d3.csv("data_filtered.csv").then((importedData) => {
+    var data = importedData;
+
+    console.log(data);
+
+    var tbody = d3.select("tbody");
+
+data.forEach((data) => {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+})
+
